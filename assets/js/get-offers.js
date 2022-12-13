18de0ae8472f6f50ebd85d87747a8cea2282fc9b
Gauthier <g23e981@gmail.com>
2022-12-13 13:01:03 -0500

    Update get-offers.js

--- assets/js/get-offers.js
+++ assets/js/get-offers.js
@@ -419,8 +419,10 @@ function recordUserLike(offerId, cardAccount, data) {
     let url = "https://triple-proxy.grogoo.dev/user-preferences";
     let currentlikes = data.liked_offers;
     let newLikes = currentlikes.push(offerId.toString());
+    console.log("CURRENT LIKES");
     console.log(currentlikes);
-    console.log(newLikes)
+    console.log("NEW LIKES");
+    console.log(newLikes);
     const currentDate = new Date(Date.now());
     let isoDate = currentDate.toISOString();
     let body = {
